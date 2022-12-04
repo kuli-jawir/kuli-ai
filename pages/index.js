c2ef852b523b2ccb55c6c477f62aaea97f2c39f0
kuli-jawir <119807691+kuli-jawir@users.noreply.github.com>
2022-12-05 00:16:33 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,57 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+const [userInput, setUserInput] = useState('');
+const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+const onUserChangedText = (event) => {
+  console.log(event.target.value);
+  setUserInput(event.target.value);
+ };
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Kuli AI</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Help you to use Artificial Intelligence</h2>
           </div>
         </div>
       </div>
+      <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" value={userInput} onChange={onUserChangedText} />;
+      </div>
+      <div className="prompt-buttons">
+      <a className="generate-button" onClick={callGenerateEndpoint}>
+        <div className="generate">
+          <p >Generate</p>
+        </div>
+      </a>
+  </div>
       <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
@@ -26,7 +60,7 @@ const Home = () => {
         >
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>build by Kuli</p>
           </div>
         </a>
       </div>
